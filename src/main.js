813362b28594a9fcbe658e6d86854f3cdd5a1987
kwak chae won <74225015+kwakchaewon@users.noreply.github.com>
2024-02-14 11:13:57 +0900

    게시판 목록 API 연동

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
 
 import './assets/common.css'
 
 const app = createApp(App)
+app.config.globalProperties.$axios = axios; // 전역변수로 설정 컴포넌트에서 this.$axios 호출
+app.config.globalProperties.$serverUrl = '//localhost:8080' // API 서버
 app.use(router).mount('#app')
--- src/views/BoardList.vue
+++ src/views/BoardList.vue
@@ -8,16 +8,12 @@
       <tr>
         <th>No</th>
         <th>제목</th>
-        <th>작성자</th>
-        <th>등록일시</th>
       </tr>
       </thead>
       <tbody>
-      <tr v-for="(row, idx) in list" :key="idx">
-        <td>{{ row.idx }}</td>
-        <td><a v-on:click="fnView(`${row.idx}`)">{{ row.title }}</a></td>
-        <td>{{ row.author }}</td>
-        <td>{{ row.created_at }}</td>
+      <tr v-for="(row, id) in list" :key="id">
+        <td>{{ row.id }}</td>
+        <td><a v-on:click="fnView(`${row.id}`)">{{ row.title }}</a></td>
       </tr>
       </tbody>
     </table>
@@ -79,27 +75,25 @@ export default {
   },
   methods: {
     fnGetList() {
-      this.list = [
-        {
-            "idx":1,
-            "title": "제목1",
-            "author": "작성자1",
-            "created_at": "작성일시1"
-        },
-        {
-            "idx":1,
-            "title": "제목1",
-            "author": "작성자1",
-            "created_at": "작성일시1"
-        },
-        {
-            "idx":1,
-            "title": "제목1",
-            "author": "작성자1",
-            "created_at": "작성일시1"
-        }
-      ]
+    this.requestBody = { // 데이터 전송
+            keyword: this.keyword,
+            page: this.page,
+            size: this.size
+          }
+
+          this.$axios.get(this.$serverUrl + "/board/list", {
+            params: this.requestBody,
+            headers: {}
+          }).then((res) => {
+
+            this.list = res.data  //서버에서 데이터를 목록으로 보내므로 바로 할당하여 사용할 수 있다.
+
+          }).catch((err) => {
+            if (err.message.indexOf('Network Error') > -1) {
+              alert('네트워크가 원활하지 않습니다.\n잠시 후 다시 시도해주세요.')
+            }
+          })
     }
-    }
+   }
 }
 </script>
