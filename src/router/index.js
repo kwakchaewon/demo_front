0cc527fbd3119f5951b79314fe4bcee686c87586
kwak chae won <74225015+kwakchaewon@users.noreply.github.com>
2024-02-14 10:40:09 +0900

    게시판 목록 레이아웃 구성

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import PageHome from '@/views/PageHome.vue'
+import BoardList from '@/views/BoardList.vue'
 
 const routes = [
   {
@@ -14,7 +15,13 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/PageAbout.vue')
-  }
+  },
+   {
+      path: '/board/list',
+      name: 'BoardList',
+      component: BoardList
+    }
+
 ]
 
 const router = createRouter({
