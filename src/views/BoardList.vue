48cf7e73440aa3c5b6c525ebddee79e32d34ed30
kwak chae won <74225015+kwakchaewon@users.noreply.github.com>
2024-02-15 16:15:19 +0900

    게시판 목록 페이징 적용

--- src/views/BoardList.vue
+++ src/views/BoardList.vue
@@ -18,26 +18,28 @@
       </tr>
       </tbody>
     </table>
+
+    <div class="pagination w3-bar w3-padding-16 w3-small" v-if="paging.totalListCnt > 0">
+          <span class="pg">
+          <a href="javascript:;" @click="fnPage(1)" class="first w3-button w3-border">&lt;&lt;</a>
+          <a href="javascript:;" v-if="paging.startPage > 10" @click="fnPage(`${paging.startPage-1}`)"
+             class="prev w3-button w3-border">&lt;</a>
+          <template v-for=" (n,index) in paginavigation()">
+              <template v-if="paging.page==n">
+                  <strong class="w3-button w3-border w3-green" :key="index">{{ n }}</strong>
+              </template>
+              <template v-else>
+                  <a class="w3-button w3-border" href="javascript:;" @click="fnPage(`${n}`)" :key="index">{{ n }}</a>
+              </template>
+          </template>
+          <a href="javascript:;" v-if="paging.totalPageCnt > paging.endPage"
+             @click="fnPage(`${paging.endPage+1}`)" class="next w3-button w3-border">&gt;</a>
+          <a href="javascript:;" @click="fnPage(`${paging.totalPageCnt}`)" class="last w3-button w3-border">&gt;&gt;</a>
+          </span>
+    </div>
+
     <div class="common-buttons">
           <button type="button" class="w3-button w3-round w3-blue-gray" v-on:click="fnWrite">등록</button>
-    </div>
-    <div class="pagination w3-bar w3-padding-16 w3-small" v-if="paging.total_list_cnt > 0">
-      <span class="pg">
-      <a href="javascript:;" @click="fnPage(1)" class="first w3-button w3-border">&lt;&lt;</a>
-      <a href="javascript:;" v-if="paging.start_page > 10" @click="fnPage(`${paging.start_page-1}`)"
-         class="prev w3-button w3-border">&lt;</a>
-      <template v-for=" (n,index) in paginavigation()">
-          <template v-if="paging.page==n">
-              <strong class="w3-button w3-border w3-green" :key="index">{{ n }}</strong>
-          </template>
-          <template v-else>
-              <a class="w3-button w3-border" href="javascript:;" @click="fnPage(`${n}`)" :key="index">{{ n }}</a>
-          </template>
-      </template>
-      <a href="javascript:;" v-if="paging.total_page_cnt > paging.end_page"
-         @click="fnPage(`${paging.end_page+1}`)" class="next w3-button w3-border">&gt;</a>
-      <a href="javascript:;" @click="fnPage(`${paging.total_page_cnt}`)" class="last w3-button w3-border">&gt;&gt;</a>
-      </span>
     </div>
   </div>
   </section>
@@ -53,25 +55,25 @@ export default {
       no: '', //게시판 숫자처리
       paging: {
         block: 0,
-        end_page: 0,
-        next_block: 0,
+        endPage: 0,
+        nextBlock: 0,
         page: 0,
-        page_size: 0,
-        prev_block: 0,
-        start_index: 0,
-        start_page: 0,
-        total_block_cnt: 0,
-        total_list_cnt: 0,
-        total_page_cnt: 0,
+        pageSize: 0,
+        prevBlock: 0,
+        startIndex: 0,
+        startPage: 0,
+        totalBlockCnt: 0,
+        totalListCnt: 0,
+        totalPageCnt: 0,
       }, //페이징 데이터
       page: this.$route.query.page ? this.$route.query.page : 1,
       size: this.$route.query.size ? this.$route.query.size : 10,
       keyword: this.$route.query.keyword,
       paginavigation: function () { //페이징 처리 for문 커스텀
         let pageNumber = [] //;
-        let start_page = this.paging.start_page;
-        let end_page = this.paging.end_page;
-        for (let i = start_page; i <= end_page; i++) pageNumber.push(i);
+        let startPage = this.paging.startPage;
+        let endPage = this.paging.endPage;
+        for (let i = startPage; i <= endPage; i++) pageNumber.push(i);
         return pageNumber;
       }
     }
@@ -91,8 +93,15 @@ export default {
         params: this.requestBody,
         headers: {}
       }).then((res) => {
+        if (res.data.resultCode === "OK") {
+          this.list = res.data.data
+          this.paging = res.data.pagination
+          this.no = this.paging.totalListCnt - ((this.paging.page - 1) * this.paging.pageSize)
 
-        this.list = res.data  //서버에서 데이터를 목록으로 보내므로 바로 할당하여 사용할 수 있다.
+          console.log(this.paging.page)
+          console.log(this.paging.pageSize)
+          console.log(this.paging.totalListCnt)
+        }
 
       }).catch((err) => {
         if (err.message.indexOf('Network Error') > -1) {
@@ -100,9 +109,9 @@ export default {
         }
       })
     },
-    fnView(idx) {
+    fnView(id) {
       this.$router.push({
-        path: '/board/'+idx,
+        path: '/board/'+id,
       })
     },
     fnWrite() {
