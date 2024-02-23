8a183600aae41d7388b373d8b3302985c7d3211d
kwak chae won <74225015+kwakchaewon@users.noreply.github.com>
2024-02-23 14:49:06 +0900

    화면가입 프론트단 구현

--- src/views/common/Regist.vue
+++ src/views/common/Regist.vue
@@ -10,14 +10,14 @@
                 <input class="w3-input" name="userId" placeholder="ID 를 입력하세요." v-model="userId"><br>
               </p>
               <p>
-                <input name="password1" class="w3-input" placeholder="비밀번호를 입력하세요." id="password" v-model="password"
-                       type="password">
+                <input name="userPw" class="w3-input" placeholder="비밀번호를 입력하세요." id="userPw" v-model="userPw"
+                       type="userPw">
               </p>
               <p>
                 <input name="password2" class="w3-input" placeholder="비밀번호를 확인하세요." type="password">
               </p>
               <p>
-                <input class="w3-input" name="email" placeholder="이메일을 입력하세요." id="email" v-model="userName"><br>
+                <input class="w3-input" name="email" placeholder="이메일을 입력하세요." id="email" v-model="email"><br>
               </p>
               <p>
                 <button type="submit" class="w3-button w3-green w3-round" v-on:click="fnRegist">가입</button>
@@ -38,28 +38,26 @@ export default {
   data() {
     return {
       userId: '',
-      paswword1: '',
-      paswword2: '',
+      userPw: '',
       email: ''
     }
   },
 
   methods: {
     submitForm() {
-      let apiUrl = 'localhost:8080' + '/member/regist'
-      console.log(apiUrl)
-
       this.form = {
         "userId": this.userId,
-        "password1": this.password1,
-        "password2": this.password2,
+        "userPw": this.userPw,
         "email": this.email
       }
 
-      this.$axios.post(apiUrl, this.form)
-          .then((res) => {
-            alert('회원가입 완료')
-            this.$router.push(this.$serverUrl + res.data.id)
+      console.log(this.$serverUrl+'/member/regist')
+
+
+      this.$axios.post(this.$serverUrl+'/member/regist',this.form)
+          .then(() => {
+            alert('회원가입이 완료되었습니다. 로그인 화면으로 이동합니다.')
+            this.$router.push('/member/login')
           }).catch((err) => {
         if (err.message.indexOf('Network Error') > -1) {
           alert('네트워크가 원활하지 않습니다.\n잠시 후 다시 시도해주세요.')
