b213f973b76eb7a0329c8b1aaf3999a8a27a620c
kwak chae won <74225015+kwakchaewon@users.noreply.github.com>
2024-03-18 15:58:54 +0900

    회원가입시 유효성 조건문 추가

--- src/views/common/SignUp.vue
+++ src/views/common/SignUp.vue
@@ -6,9 +6,7 @@
           <h2>회원가입</h2>
           <div id="loginForm">
             <form @submit.prevent="submitForm">
-              <p>
                 <input class="w3-input" name="userId" placeholder="ID 를 입력하세요." v-model="userId"><br>
-              </p>
               <p>
                 <input name="userPw" class="w3-input" placeholder="비밀번호를 입력하세요." id="userPw" v-model="userPw"
                        type="password">
@@ -20,6 +18,10 @@
               <p>
                 <input class="w3-input" name="email" placeholder="이메일을 입력하세요." id="email" v-model="email"><br>
               </p>
+
+              <p style="text-align: left">아이디 조건: 영문 또는 숫자</p>
+              <p style="text-align: left">비밀번호 조건: 8글자 이상 영문, 숫자, 특수문자 조합 비밀번호</p>
+              <br>
               <p>
                 <button type="submit" class="w3-button w3-green w3-round" v-on:click="fnRegist">가입</button>
               </p>
