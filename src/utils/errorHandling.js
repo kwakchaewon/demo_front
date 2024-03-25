f1089929f7ce3cfca2bc5d6e7a7b74447cdcfe63
kwak chae won <74225015+kwakchaewon@users.noreply.github.com>
2024-03-25 17:23:31 +0900

    예외처리모듈 생성

---
+++ src/utils/errorHandling.js
@@ -0,0 +1,21 @@
+/**
+ *  에러 log 출력 및 알림창 띄우기
+ */
+export function handleErrorWithAlert(err) {
+    if (err.response && err.response.data && err.response.data.message) {
+        const errorMessage = err.response.data.message;
+        console.log(errorMessage);
+        alert(errorMessage);
+    } else {
+        console.error('에러가 발생했습니다:', err);
+        alert('알 수 없는 에러가 발생했습니다.');
+    }
+}
+
+/**
+ * 알 수 없는 에러 발생 핸들링
+ */
+export function handleAnonymousError(err) {
+    console.error('에러가 발생했습니다:', err);
+    alert('알 수 없는 에러가 발생했습니다.');
+}
