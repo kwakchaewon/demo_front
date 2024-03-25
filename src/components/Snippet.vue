d0e560ecc32a9058310edaf8b1076d4d369f3c86
kwak chae won <74225015+kwakchaewon@users.noreply.github.com>
2024-03-25 18:24:56 +0900

    snippet 템플릿 생성

---
+++ src/components/Snippet.vue
@@ -0,0 +1,28 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: '', // 컴포넌트 이름
+  components: {}, // 다른 컴포넌트 사용시 컴포넌트 import 하고 배열로 저장
+  data(){
+    return {
+      sampleData: ''
+    };
+  },
+
+  setup(){}, // 컴포지션 api
+  created() {}, // 컴포넌트 생성시 실행
+  mounted() {}, // 데이터 바인딩 단계서 실행
+  unmounted() {}, //
+  methods: {} // 컴포넌트 내 메서드 정의
+}
+
+</script>
+
+
+
+<style scoped>
+
+</style>
