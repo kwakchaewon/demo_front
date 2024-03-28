f2f42e00a7df147986ac820d904d42792076435a
kwak chae won <74225015+kwakchaewon@users.noreply.github.com>
2024-03-28 17:47:54 +0900

    댓글 수정시 url 변동없이 댓글 창에서 추가

--- src/components/Comment.vue
+++ src/components/Comment.vue
@@ -3,32 +3,41 @@
     <h5 class="border-bottom my-3 py-2">댓글</h5>
     <div class="card my-3" v-for="(row, id) in list" :key="id">
       <div class="card-body">
-        <div class="card-text" style="white-space: pre-line;">{{ row.contents }}</div>
+        <div v-if="row.editing">
+          <textarea v-model="row.editContent"></textarea>
+        </div>
+        <div v-else class="card-text" style="white-space: pre-line;">{{ row.contents }}</div>
+        <br>
         <div class="d-flex justify-content-end">
-          <div class="badge bg-light text-dark p-2 text-start mx-3">
+          <div class="badge bg-light text-dark p-2 text-start mx-3" v-if="!row.editing">
             <div class="mb-2">작성자</div>
             <div>{{ row.memberId }}</div>
           </div>
-          <div class="badge bg-light text-dark p-2 text-start mx-3">
+          <div class="badge bg-light text-dark p-2 text-start mx-3" v-if="!row.editing">
             <div class="mb-2">작성일</div>
             <div>{{ row.createdAt }}</div>
           </div>
-          <div v-if="row.updatedAt" class="badge bg-light text-dark p-2 text-start mx-3">
+          <div class="badge bg-light text-dark p-2 text-start mx-3" v-if="row.updatedAt && !row.editing">
             <div class="mb-2">수정일</div>
             <div>{{ row.updatedAt }}</div>
           </div>
         </div>
         <div class="my-3">
-          <button class="btn btn-sm btn-outline-secondary" v-on:click="fnCheckUpdate(row.id)"
-                  v-if="row.memberId == this.$store.state.user ">수정
+          <button class="btn btn-sm btn-outline-secondary" v-on:click="toggleEdit(row)"
+                  v-if="row.memberId == this.$store.state.user && !row.editing">수정
+          </button>
+          <button class="btn btn-sm btn-outline-secondary" v-on:click="updateComment(row)"
+                  v-if="row.memberId == this.$store.state.user && row.editing">수정
+          </button>
+          <button class="btn btn-sm btn-outline-secondary" v-on:click="toggleEditCancel(row)"
+                  v-if="row.memberId == this.$store.state.user && row.editing">취소
           </button>
           <button class="delete btn btn-sm btn-outline-secondary" v-on:click="fnDelete(`${row.id}`)"
-                  v-if="row.memberId == this.$store.state.user ">삭제
+                  v-if="row.memberId == this.$store.state.user && !row.editing">삭제
           </button>
         </div>
       </div>
     </div>
-
     <div class="card my-3">
       <div class="board-contents">
       <textarea id="contents" cols="30" rows="3" v-model="contents" class="w3-input w3-border" style="resize: none;"
@@ -59,8 +68,63 @@ export default {
     this.fnGetview();
   },
   methods: {
-    fnList() {
-      this.$router.push({path: '/board',});
+    toggleEdit: function (row) {
+      row.editing = !row.editing; // 수정 모드 토클
+      if (row.editing) {
+        row.editContent = row.contents; // 수정 내용을 기존내용으로 초기화
+      }
+    },
+
+    toggleEditCancel: function (row) {
+      row.editing = !row.editing; // 수정 모드 토클
+    },
+
+    updateComment(row){
+      const commentId = row.id;
+
+      this.form = {
+        "contents": row.editContent
+      };
+
+      const blankPattern = /^\s*$/ // 공백 유효성 검사
+
+      if (blankPattern.test(row.editContent)) {
+        alert("빈 내용은 입력할 수 없습니다.")
+      } else {
+        this.$axios.put(this.$serverUrl + `/comment/${commentId}`, this.form)
+            .then((res) => {
+              alert('댓글이 수정되었습니다.');
+              row.editContent = res.data.contents; // editContent 에 res.data.contents 업데이트
+              row.contents = row.editContent; // 수정된 내용을 표시 중인 내용으로 업데이트
+              row.editing = false; // 수정 모드 종료
+              this.fnGetview();
+            }).catch((err) => {
+
+          // COMMENT_NOTFOUND: 댓글 부재시, alert 반환
+          if (err.response.data.status === "404" && err.response.data.message) {
+            console.log(err.response.data.message);
+            alert(err.response.data.message);
+          }
+
+          // 403 권한없음 예외 처리
+          else if (err.response.data.status === 403) {
+            console.log(err.message);
+            alert("수정 권한이 없습니다.")
+          }
+
+          // 그 외 Custom Exception 발생시 alert 반환
+          else if (err.response.data.status && err.response.data.message) {
+            console.log(err.response.data.message);
+            alert(err.response.data.message);
+          }
+
+          // 기타
+          else {
+            console.log('댓글 수정에 실패했습니다.');
+            alert('댓글 수정에 실패했습니다.');
+          }
+        })
+      }
     },
 
     // 댓글 리스트
@@ -218,5 +282,8 @@ export default {
 }
 </script>
 <style scoped>
-
+textarea {
+  resize:none;
+  width: 100%;
+}
 </style>
