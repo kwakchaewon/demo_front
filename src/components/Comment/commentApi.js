774bca0baaea4ed5d178eea300c0a3e802121336
kwak chae won <74225015+kwakchaewon@users.noreply.github.com>
2024-04-02 16:19:19 +0900

    불필요한 코드 삭제

--- src/components/Comment/commentApi.js
+++ src/components/Comment/commentApi.js
@@ -32,15 +32,12 @@ export default {
     saveComment(boardId, form){
         return axios.post(serverUrl + '/board/' + boardId + '/comment', form)
             .then((res)=>{
-
-                console.log(res.data);
                 // 게시글 등록 성공
                 if(res.data.state.statusCode === 200){
                 }
 
                 // 게시글 부재시
                 else if(res.data.state.statusCode === 404){
-                    console.log(11);
                     throw new Error("게시글을 찾을 수 없습니다.");
                 }
 
